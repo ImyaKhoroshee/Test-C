d2db0ba33c2c550a4a955120de2e6f60fc24a382
tatyana <tilkka@mail.ru>
2022-02-23 23:19:54 +0300

    Example004 division: int+double

---
+++ Example004/Program.cs
@@ -0,0 +1,13 @@
+﻿With Integers
+
+Int NumberA = 10;
+Int NumberB = 5;
+Console.WriteLine(NumberA / NumberB);
+
+or
+
+With doubles
+
+Double NumberA = 12;
+Double NumberB = 5;
+Console.WriteLine(NumberA / NumberB);
