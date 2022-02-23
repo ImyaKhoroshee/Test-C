48b619e706b957262673abbfd1b984d59654d2b4
tatyana <tilkka@mail.ru>
2022-02-23 22:33:41 +0300

    Example003 has been added

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿int NumberA = 3;
+int NumberB = 5;
+Console.WriteLine(NumberA + NumberB);
+
+or
+
+int NumberA = 3;
+int NumberB = 5;
+int result = (NumberA + NumberB);
+Console.WriteLine(NumberA + NumberB);
